3f14d3dac009b8462c024b41199e5b3a30c06bd9
Apps-Factory-Colombia <appsfactorycolombia@gmail.com>
2026-06-24 17:03:42 -0500

    feat: enable open cors for api

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -5,6 +5,10 @@ import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
